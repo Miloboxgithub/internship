76fd393709813482b401b587b298adbb908360a4
Roxy <1012436741@qq.com>
2024-09-01 22:49:22 +0800

    9/1

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,29 +1,30 @@
 Component({
   data: {
-    selected: 0,
+    selected:0,
     color: "#808080",
     selectedColor: "#388BFF",
     list: [{
-      pagePath: "pages/index/index",
-      iconPath: "/asset/1.png",
-      selectedIconPath: "/asset/11.png",
-      text: "实习咨讯"
+      "pagePath": "/pages/home/home",
+      "iconPath": "/img/1.png",
+      "selectedIconPath": "/img/11.png",
+      "text": "实习咨讯"
     }, {
-      pagePath: "pages/publish/publish",
-      iconPath: "/asset/2.png",
-      selectedIconPath: "/asset/22.png",
-      text: "实习发布"
+      "pagePath": "/pages/publish/publish",
+      "iconPath": "/img/2.png",
+      "selectedIconPath": "/img/22.png",
+      "text": "实习发布"
     }
     , {
-      pagePath: "pages/person/person",
-      iconPath: "/asset/3.png",
-      selectedIconPath: "/asset/33.png",
-      text: "个人中心"
+      "pagePath": "/pages/person/person",
+      "iconPath": "/img/3.png",
+      "selectedIconPath": "/img/33.png",
+      "text": "个人中心"
     }]
   },
   attached() {
   },
   methods: {
+    
     switchTab(e) {
       const data = e.currentTarget.dataset
       const url = data.path
