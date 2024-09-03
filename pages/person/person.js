2452e57f4bc561833bf7a296eafc23dfbfd3a6d1
Roxy <1012436741@qq.com>
2024-09-03 22:54:57 +0800

    9-3

--- pages/person/person.js
+++ pages/person/person.js
@@ -1,15 +1,22 @@
 Page({
   data: {
-    display:''
+    tabs: [
+      { id: '1', name: '游戏设计' },
+      { id: '2', name: '机械设计' },
+      { id: '3', name: '工业设计' },
+      { id: '4', name: '互联网' },
+      { id: '5', name: '影视行业' },
+      { id: '6', name: '人工智能' },
+      { id: '7', name: '大数据' }
+    ],
+    activeTabId: '1',
+    scrollIntoViewId: 'tab1'
   },
-  showview: function() { 
+  onTabTap: function(e) {
+    const id = e.currentTarget.dataset.id;
     this.setData({
-      display: "block"
-    })
-  },
-  hideview: function() {
-    this.setData({
-      display: "none"
-    })
+      activeTabId: id,
+      scrollIntoViewId: `tab${id}`
+    });
   }
-})
+});
