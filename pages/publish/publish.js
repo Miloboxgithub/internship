df1333d0dc321ae44c1e1ac4dc28c27fd3df49bb
Roxy <1012436741@qq.com>
2024-09-11 11:19:41 +0800

    9-11 morning

--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -2,20 +2,22 @@
 Page({
   data: {
     scrollTop: 0,
+    types0: false,
+    checks:false
   },
   onShow: function () {
     if (typeof this.getTabBar === 'function' &&
-    this.getTabBar()) {
-    this.getTabBar().setData({
-      selected: 1
-    })
-  }
+      this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 1
+      })
+    }
   },
-  onScrollToLower: function(e) {
+  onScrollToLower: function (e) {
     this.setData({
       scrollTop: e.detail.scrollTop
     });
-    
+
     // 当页面滚动时，调整导航栏的位置
     if (e.detail.scrollTop > 44) { // 导航栏高度
       wx.createSelectorQuery().select('#navBar').boundingClientRect((rect) => {
@@ -28,5 +30,15 @@ Page({
         navTop: 0
       });
     }
+  },
+  changetypes: function () {
+    this.setData({
+      types0: !this.data.types0
+    })
+  },
+  changecheck: function(){
+    this.setData({
+      checks:!this.data.checks
+    })
   }
 });
